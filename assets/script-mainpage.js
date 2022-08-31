f4872ac5ecb8665824f98cbd5f635ff0e0833b2a
MortexAG <107272049+MortexAG@users.noreply.github.com>
2022-08-31 01:54:06 +0000

    more changes

--- assets/script-mainpage.js
+++ assets/script-mainpage.js
@@ -3,7 +3,9 @@ function light_mode() {
   var body_element = document.body;
   body_element.classList.toggle("body_light_mode");
   var top_div = document.getElementById("top_div");
-  top_div.classList.toggle("top_div_light_mode")
+  top_div.classList.toggle("top_div_light_mode");
+  var mid_div = document.getElementById("mid_div");
+  mid_div.classList.toggle("top_div_light_mode");
   var change_icon = document.getElementById("dark_mode_moon");
   let url = document.getElementById("dark_mode_moon").src;
   if (url == 'https://i.ibb.co/RvzBsgn/180700.png') {
@@ -12,6 +14,12 @@ function light_mode() {
   }
   else {
     document.getElementById('dark_mode_moon').src = 'https://i.ibb.co/RvzBsgn/180700.png'
+    document.getElementById("dark_mode_moon").title = "Switch To Light Mode"
   }
 
+}
+
+function button_active(){
+  var active_now = document.getElementById("but_active");
+  active_now.classList.toggle("inactive");
 }
