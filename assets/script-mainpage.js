dcdf44c4a6d7855e6aca42717ceed9f532667931
MortexAG <107272049+MortexAG@users.noreply.github.com>
2022-08-31 00:08:06 +0000

    added light and dark mode switching

---
+++ assets/script-mainpage.js
@@ -0,0 +1,17 @@
+/* Function To Switch Dark Mode To Light Mode */
+function light_mode() {
+  var body_element = document.body;
+  body_element.classList.toggle("body_light_mode");
+  var top_div = document.getElementById("top_div");
+  top_div.classList.toggle("top_div_light_mode")
+  var change_icon = document.getElementById("dark_mode_moon");
+  let url = document.getElementById("dark_mode_moon").src;
+  if (url == 'https://i.ibb.co/RvzBsgn/180700.png') {
+    document.getElementById("dark_mode_moon").src = 'https://i.ibb.co/WVX5f3b/brightness.png'
+    document.getElementById("dark_mode_moon").title = "Switch To Dark Mode"
+  }
+  else {
+    document.getElementById('dark_mode_moon').src = 'https://i.ibb.co/RvzBsgn/180700.png'
+  }
+
+}
